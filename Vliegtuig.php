e180d1ad5d22272191a66b8719f093b33e99cf3c
Pinda_Man <rejj.aue@gmail.com>
2020-02-07 11:43:18 +0100

    [de code wat uigebreider gemaakt]

--- Vliegtuig.php
+++ Vliegtuig.php
@@ -2,9 +2,23 @@
 <html>
 <head>
     <title>Groot Voorhoofd</title>
-    <link rel="stylesheet" href="Vliegtuig.css">
+    <link rel="stylesheet" href="Vliegtuig1.css">
 </head>
 <body>
-<h1>je</h1>
+
+<h1>OnTheFly</h1>
+
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+  <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
+  </div>
+</div>
+
 </body>
 </html>
