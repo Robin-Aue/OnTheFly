dcdf1cba914e22b30eff45c87c5b5b1d907f4257
Pinda_Man <rejj.aue@gmail.com>
2020-02-12 08:54:03 +0100

    een paar anpassingen

---
+++ inplannen.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Groot Voorhoofd</title>
+    <link rel="stylesheet" href="Vliegtuig1.css">
+</head>
+<body>
+
+<h1>OnTheFly</h1>
+
+<div class="topnav">
+  <a class="active" href="vliegtuig.php">Home</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+  <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
+  </div>
+</div>
+
+</body>
+</html>
